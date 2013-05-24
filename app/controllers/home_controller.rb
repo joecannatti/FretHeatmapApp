0f9623c1421ad20d827f54e76f93b03c1e16649e
Joe Cannatti <joecannatti@gmail.com>
2013-05-24 18:10:12 -0400

    Look good on mobile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
-  helper_method :class_for_action
+  helper_method :class_for_action, :mobile_device?
 
   def index
     @fretboards = Fretboard.all
@@ -8,5 +8,13 @@ class HomeController < ApplicationController
 
   def class_for_action(action)
     action == params[:action] ? "current" : ""
+  end
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
   end
 end
